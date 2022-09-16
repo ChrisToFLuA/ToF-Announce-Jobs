bfbd2f0400cd610f85597c14ade08ade6bdeaaf8
ChrisToF29380 <109798768+ChrisToFLuA@users.noreply.github.com>
2022-09-16 10:37:32 +0200

    secure trigger
    secure trigger, move loop jobs server side

--- client/clientaj.lua
+++ client/clientaj.lua
@@ -22,55 +22,17 @@ RegisterCommand(namecommand, function(source, args, raw)
 end)
 
 RegisterNetEvent('ajob:allannonce')
-AddEventHandler('ajob:allannonce', function(job, text)
-    local textann = text
-    publishAnn(textann, job)
+AddEventHandler('ajob:allannonce', function(textann, titlea, pos, timer, bgc, ctxt, icont, iconc)
+    lib.notify({
+        title = titlea,
+        description = textann,
+        position = pos,
+        duration = timer,
+        style = {
+        backgroundColor = bgc,
+        color = ctxt
+        },
+    icon = icont,
+    iconColor = iconc
+    })
 end)
-
-function publishAnn(textann, job)
-    --------------------------------------------------------------------------------------------------------------
-    -- add other jobs below in local jobC, copy / paste lines
-    ----------------------------------------------------------
-    ----------------------------------------------------------
-    -- nameJ : name of the job in sql
-    -- labelJ : Title of the announce
-    -- postionJ : 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
-    -- durationJ : time in ms the message will be viewed
-    -- for the colors fields use link : https://htmlcolorcodes.com/fr/ - and use HEX to copy / paste on the fields
-    -- iconJ : use link https://fontawesome.com/search?o=r&f=sharp
-    --------------------------------------------------------------------------------------------------------------
-    local jobC = {
-        {nameJ = 'police', labelJ = 'LSPD', positionJ = 'top-right', durationJ = 5000, bgcolor = '#4B26D3', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        {nameJ = 'ambulance', labelJ = 'EMS', positionJ = 'top-right', durationJ = 5000, bgcolor = '#0193F8', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        {nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
-        
-    }
-    --------------------------------------------------------------------------------------------------------------
-    --------------------------------------------------------------------------------------------------------------
-    for c = 1,#jobC,1 do
-        if job == jobC[c].nameJ then
-            lib.notify({
-                title = jobC[c].labelJ,
-                description = textann,
-                position = positionJ,
-                duration = durationJ,
-                style = {
-                backgroundColor = bgcolor,
-                color = txtcolor
-                },
-            icon = iconJ,
-            iconColor = iconcolorJ
-            })
-        end
-    end
-    --------------------------------------------------------------------------------------------------------------
-end
--- server/serveraj.lua
+++ server/serveraj.lua
@@ -10,38 +10,46 @@ AddEventHandler('ajob:verifjob', function(text)
     --------------------------- list of jobs --------------------------
     -------------------------------------------------------------------
 
-    local jobS = { -- add here the jobsname like in sql (not the label)
-        {name = 'police'},            
-        {name = 'ambulance'},
-        {name = 'mechanic'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-        --{name = 'jobname'},
-       }
-
+    --------------------------------------------------------------------------------------------------------------
+    -- add other jobs below in local jobC, copy / paste lines
+    ----------------------------------------------------------
+    ----------------------------------------------------------
+    -- nameJ : name of the job in sql
+    -- labelJ : Title of the announce
+    -- postionJ : 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left'
+    -- durationJ : time in ms the message will be viewed
+    -- for the colors fields use link : https://htmlcolorcodes.com/fr/ - and use HEX to copy / paste on the fields
+    -- iconJ : use link https://fontawesome.com/search?o=r&f=sharp
+    --------------------------------------------------------------------------------------------------------------
+    local jobC = {
+        {nameJ = 'police', labelJ = 'LSPD', positionJ = 'top-right', durationJ = 5000, bgcolor = '#4B26D3', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        {nameJ = 'ambulance', labelJ = 'EMS', positionJ = 'top-right', durationJ = 5000, bgcolor = '#0193F8', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        {nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        --{nameJ = 'mechanic', labelJ = 'LS CUSTOM', positionJ = 'top-right', durationJ = 5000, bgcolor = '#FF8300', txtcolor = '#FFFFFF', iconJ = 'circle-info', iconcolorJ = '#FFFFFF'},
+        
+    }
     -------------------------------------------------------------------
     ------------------------ end of the list --------------------------
     -------------------------------------------------------------------
 
-    for s = 1,#jobS,1 do
-        if jobP == jobS[s].name then
-            TriggerEvent('ajob:allannonce', jobP, text)
+    for s = 1,#jobC,1 do
+        if jobP == jobC[s].nameJ then
+            local title = jobC[s].labelJ
+            local pos = jobC[s].positionJ
+            local timer = jobC[s].durationJ
+            local bgc = jobC[s].bgcolor
+            local ctxt = jobC[s].txtcolor
+            local icont = jobC[s].iconJ
+            local iconc = jobC[s].iconcolorJ
+            TriggerClientEvent('ajob:allannonce', -1, text, title, pos, timer, bgc, ctxt, icont, iconc)
         end
     end
-end)
-
-RegisterServerEvent('ajob:allannonce')
-AddEventHandler('ajob:allannonce', function(job, text)
-    TriggerClientEvent('ajob:allannonce', -1, job, text)    
-    print(text)
 end)
